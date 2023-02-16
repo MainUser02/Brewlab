1fc3378114e9aea03b8fc37db7c281a7bcd58f3f
MainUser02 <adilkhan.nurdaulet.99@gmail.com>
2023-02-16 18:42:30 +0600

    6 commit

--- js/slider.js
+++ js/slider.js
@@ -9,7 +9,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
 const prevBtn = document.querySelector(".prev");

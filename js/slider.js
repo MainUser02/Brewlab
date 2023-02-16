e89592cbc8d4b0d012990557b95eeb2f2eed7cbc
MainUser02 <adilkhan.nurdaulet.99@gmail.com>
2023-02-16 17:46:07 +0600

    4 commit

---
+++ js/slider.js
@@ -0,0 +1,24 @@
+﻿let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+prevBtn.addEventListener("click", () => {
+  slideIndex-1;
+  showSlides();
+});
+nextBtn.addEventListener("click", () => {
+  slideIndex+1;
+  showSlides();
+});
